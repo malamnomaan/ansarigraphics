e3fe9968b68e11ef7d840d2e4cca7d1d40736e1b
Nomaan alam <nomaanalam143@gmail.com>
2025-11-29 13:30:41 +0530

    added static root in settings

--- ansarigraphics/settings.py
+++ ansarigraphics/settings.py
@@ -146,3 +146,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATICFILES_DIRS = [
  	   os.path.join(BASE_DIR, "static")
 	]
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
