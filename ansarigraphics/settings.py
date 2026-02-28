a05f7481052941a9575e97e0762b8de0a099dbc9
Adnan <adnanmohd739296@gmail.com>
2026-02-28 19:43:24 +0530

    more config

--- ansarigraphics/settings.py
+++ ansarigraphics/settings.py
@@ -53,6 +53,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -155,3 +156,4 @@ CSRF_TRUSTED_ORIGINS = [
 ]
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
